4fcf4d7a8bbb4ceecb60bfa8b2ea35e9b314b973
Alexey Lonshakov <lonshakovaleksei@gmail.com>
2021-05-16 21:36:33 +0600

    minor fixes

--- price.py
+++ price.py
@@ -1,10 +1,10 @@
 # step 7(Функции)
-# def get_summ(one, two, delimiter='&'):
-#     one =str(one).capitalize()
-#     two =str(two).capitalize()
-#     return one + delimiter + two
-# result_string = get_summ('learn', 'python')
-# print(result_string)
+def get_summ(one, two, delimiter='&'):
+    one =str(one).capitalize()
+    two =str(two).capitalize()
+    return one + delimiter + two
+result_string = get_summ('learn', 'python')
+print(result_string)
 
 def format_price(price):
     price = int(price)
